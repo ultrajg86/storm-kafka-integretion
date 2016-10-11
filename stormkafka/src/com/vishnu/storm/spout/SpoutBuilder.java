360d14f13abefdf80814c46b932feebedf91129f
JongGab <ultrajg86@gmail.com>
2016-10-11 21:08:30 +0900

    asdfasdfasdf

---
+++ stormkafka/src/com/vishnu/storm/spout/SpoutBuilder.java
@@ -0,0 +1,35 @@
+package com.vishnu.storm.spout;
+
+import java.util.Properties;
+
+import org.apache.storm.kafka.BrokerHosts;
+import org.apache.storm.kafka.KafkaSpout;
+import org.apache.storm.kafka.SpoutConfig;
+import org.apache.storm.kafka.StringScheme;
+import org.apache.storm.kafka.ZkHosts;
+import org.apache.storm.spout.SchemeAsMultiScheme;
+
+import com.vishnu.storm.Keys;
+
+/**
+ * @author vishnu viswanath
+ *
+ */
+public class SpoutBuilder {
+	
+	public Properties configs = null;
+	
+	public SpoutBuilder(Properties configs) {
+		this.configs = configs;
+	}
+	public KafkaSpout buildKafkaSpout() {
+		BrokerHosts hosts = new ZkHosts(configs.getProperty(Keys.KAFKA_ZOOKEEPER));
+		String topic = configs.getProperty(Keys.KAFKA_TOPIC);
+		String zkRoot = configs.getProperty(Keys.KAFKA_ZKROOT);
+		String groupId = configs.getProperty(Keys.KAFKA_CONSUMERGROUP);
+		SpoutConfig spoutConfig = new SpoutConfig(hosts, topic, zkRoot, groupId);
+		spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+		KafkaSpout kafkaSpout = new KafkaSpout(spoutConfig);
+		return kafkaSpout;
+	}
+}
